f65548ce2fdeacaa8b9c95a5af2dc98879627f84
Yap Han Chiang <yaphc@hotmail.com>
2019-05-19 10:25:00 +0800

    Read file input from command line argument

--- README.md
+++ README.md
@@ -15,6 +15,11 @@ Each edge is represented by the i, j entry in the adjacency matrix
 If vertices don't exist, add them in the graph and add the edges.
 If vertices exist, update the edges if `timestamp` is after the last updated timestamp for the vertices
 
+## How to use
+* Clone project: `git clone git@github.com:hanchiang/tenx-challenge.git`
+* Create an input file in project root, e.g. `input.txt`
+* Run the program and pass the input file as an argument, e.g. `cargo run input.txt`
+
 
 ## Note
 There are over 2000 cryptocurrencies and 200 crypto exchanges as of 18 May 2019
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::env;
+use std::io;
+use std::process;
+use std::fs::File;
+use std::io::Read;
+
+fn read_file(file_name: &str) ->  Result<String, io::Error> {
+    let mut file = match File::open(file_name) {
+        Ok(file) => file,
+        Err(e) => return Err(e)
+    };
+
+    let mut buffer = String::new();
+    match file.read_to_string(&mut buffer) {
+        Ok(_) => (),
+        Err(e) => return Err(e)
+    };
+    Ok(buffer)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    // println!("{:?}", args);
+
+    if args.len() != 2 {
+        println!("Usage: cargo run <input_file>, e.g. cargo run input.txt");
+        process::exit(1);
+    }
+
+    let file_content = match read_file(&args[1]) {
+        Ok(content) => content,
+        Err(e) => {
+            println!("Error encountered: {}\nExiting...", e);
+            process::exit(1);
+        }
+    };
+    println!("{:#?}", file_content);
 }
